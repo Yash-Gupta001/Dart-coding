44a33c0fb2c117c9f0d3062049dcf30deef217f2
Yash-Gupta001 <gyash4963@gmail.com>
2024-12-03 00:10:16 +0530

    Dart basics

---
+++ 1)Variables.dart
@@ -0,0 +1,11 @@
+// variables are like container which stores some values
+
+void main() {
+  int output = 23;
+  output = 35;
+  late int lateoutput;
+
+  lateoutput = 66;
+  print("output value is-: $output");
+  print("output value is-: $lateoutput");
+}
---
+++ 2)LateInDart.dart
@@ -0,0 +1,5 @@
+class LateExample {
+  int x; // Non-nullable instance field 'x' must be initialized.
+
+  late int y; // by adding late in dart we can declare
+}
